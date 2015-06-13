8ef54b5275cf1a24f0a6c8541dd209c6db1c45cb
pims <pims@park>
2015-06-13 11:11:13 -0400

    SpannableString for rich TextView.

--- app/src/main/java/com/example/ken/checksams/MainActivity.java
+++ app/src/main/java/com/example/ken/checksams/MainActivity.java
@@ -9,7 +9,16 @@ import android.os.Bundle;
 
 import android.os.Environment;
 import android.text.Html;
+import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StrikethroughSpan;
+import android.text.style.URLSpan;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -46,6 +55,8 @@ public class MainActivity extends Activity  {
 
     private WebView wv1;
     private WebView wv2;
+
+    TextView richTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,6 +98,50 @@ public class MainActivity extends Activity  {
                 new GetStringFromUrl().execute("http://pims.grc.nasa.gov/plots/user/sams/status/sensortimes.txt");
             }
         });
+
+        richTextView = (TextView) findViewById(R.id.textView1);
+
+        // this is the text we'll be operating on
+        SpannableString text = new SpannableString("Lorem ipsum dolor sit amet");
+
+        // make "Lorem" (characters 0 to 5) red
+        text.setSpan(new ForegroundColorSpan(Color.RED), 0, 5, 0);
+
+        // make "ipsum" (characters 6 to 11) one and a half time bigger than the textbox
+        text.setSpan(new RelativeSizeSpan(1.5f), 6, 11, 0);
+
+        // make "dolor" (characters 12 to 17) display a toast message when touched
+        final Context context = this;
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "dolor", Toast.LENGTH_LONG).show();
+            }
+        };
+        text.setSpan(clickableSpan, 12, 17, 0);
+
+        // make "sit" (characters 18 to 21) struck through
+        text.setSpan(new StrikethroughSpan(), 18, 21, 0);
+
+        // make "amet" (characters 22 to 26) twice as big, green and a link to this site.
+        // it's important to set the color after the URLSpan or the standard
+        // link color will override it.
+        text.setSpan(new RelativeSizeSpan(2f), 22, 26, 0);
+        text.setSpan(new URLSpan("http://www.chrisumbel.com"), 22, 26, 0);
+        text.setSpan(new ForegroundColorSpan(Color.GREEN), 22, 26, 0);
+        text.setSpan(new BackgroundColorSpan(Color.BLUE), 22, 26, 0);
+
+        SpannableString text2 = new SpannableString(" Ciao!");
+        SpannableString text3 = new SpannableString( TextUtils.concat(text, text2) );
+
+        text3.setSpan(new ForegroundColorSpan(Color.RED), 27, 31, 0);
+        text3.setSpan(new BackgroundColorSpan(Color.BLACK), 27, 31, 0);
+
+        // make our ClickableSpans and URLSpans work
+        richTextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        // shove our styled text into the TextView
+        richTextView.setText(text3, TextView.BufferType.SPANNABLE);
 
     }
 
